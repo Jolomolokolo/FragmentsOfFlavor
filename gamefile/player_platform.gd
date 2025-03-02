4ea10e634958ce50bb86eaae9a85cfa51e576e99
Jolomolokolo <julian.henryk@ziesing.net>
2025-03-02 10:18:02 +0100

    what

--- gamefile/player_platform.gd
+++ gamefile/player_platform.gd
@@ -1,28 +1,24 @@
 extends CharacterBody2D
 
-var speed = 200
-var rotation_speed = 2
+@export var speed: float = 300.0
+@export var rotation_speed: float = 3.0
 
 func _physics_process(delta):
-	velocity = Vector2.ZERO
-	
+	var direction := Vector2.ZERO
+
 	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
+		direction.x += 1
 	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
+		direction.x -= 1
 	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
+		direction.y += 1
 	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	
-	velocity = velocity.normalized() * speed
-	move_and_slide()
-	
-	if Input.is_action_pressed("rotate_right"):
-		rotation += rotation_speed * delta
+		direction.y -= 1
+
+	var motion = direction.normalized() * speed * delta
+	move_and_collide(motion)
+
 	if Input.is_action_pressed("rotate_left"):
 		rotation -= rotation_speed * delta
-	
-	for body in get_tree().get_nodes_in_group("rigid_bodies"):
-		if body is RigidBody2D:
-			body.apply_impulse(Vector2.ZERO, velocity * 10)
+	if Input.is_action_pressed("rotate_right"):
+		rotation += rotation_speed * delta
