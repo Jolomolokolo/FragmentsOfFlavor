1969fee2a5134d96c3b5dc250f130190d3cd52b4
Jolomolokolo <93781011+Jolomolokolo@users.noreply.github.com>
2025-03-10 10:47:46 +0100

    Delete site directory

--- site/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("playButton").addEventListener("click", function(event) {
-    event.preventDefault();
-    window.location.href = "https://jolomolokolo.itch.io/fragments-of-flavor";
-});
