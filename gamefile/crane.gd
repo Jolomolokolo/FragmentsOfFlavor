557c0417d416a5a1cebc82931f5ee39e4a4cdec5
Jolomolokolo <93781011+Jolomolokolo@users.noreply.github.com>
2025-03-13 23:01:39 +0100

    Crane game is getting all from me!

--- gamefile/crane.gd
+++ gamefile/crane.gd
@@ -15,42 +15,35 @@ var is_block_attached: bool = true
 var has_spawned_block: bool = false  
 var highest_block_y: float = 0
 var target_camera_position: Vector2
-
 var block_scenes = [
 	preload("res://blocks/block_32.tscn"),
 	preload("res://blocks/block_48.tscn"),
 	preload("res://blocks/block_64.tscn"),
-	preload("res://blocks/block_96.tscn")
-]
-
+	preload("res://blocks/block_96.tscn")]
+	
 func _ready():
 	start_x = position.x
 	highest_block_y = drop_point.global_position.y
 	target_camera_position = camera.position
 	print("Initiale Höhe des Turms:", highest_block_y)
-
+	
 func _process(delta):
 	if Global.desktop_crane_visible or test:
 		position.x += direction * speed * delta
-
 		if position.x >= start_x + move_range and not has_spawned_block:
 			has_spawned_block = true
 			spawn_block()
-			speed += speed_increase  
-
+			speed += speed_increase
 		if position.x > start_x + move_range:
 			direction = -1
 		elif position.x < start_x - move_range:
 			direction = 1
-			has_spawned_block = false  
-
+			has_spawned_block = false
 		if current_block and is_block_attached:
-			current_block.global_position = drop_point.global_position  
-
-	if current_block and is_block_attached:
-		check_tower_height(current_block.global_position.y)
-
-	camera.position = camera.position.lerp(target_camera_position, 0.01)
+			current_block.global_position = drop_point.global_position
+		if current_block and is_block_attached:
+			check_tower_height(current_block.global_position.y)
+		camera.position = camera.position.lerp(target_camera_position, 0.01)
 
 func spawn_block():
 	if current_block != null:
@@ -63,7 +56,7 @@ func spawn_block():
 	current_block.gravity_scale = 0  
 	is_block_attached = true  
 	print("Block instanziiert mit Y-Position: ", current_block.global_position.y)
-
+	
 func drop_block():
 	if current_block and is_block_attached:
 		is_block_attached = false
